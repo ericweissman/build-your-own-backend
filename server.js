1f8053c84f96197302c8ec4c6efdb183b8ad1ceb
Eric Weissman <eric.weissman21@gmail.com>
2019-03-14 11:40:56 -0600

    Adds post routes for players and teams

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
@@ -34,7 +35,7 @@ app.get('/api/v1/players', (request, response) => {
 
 app.get('/api/v1/teams/:id', (request, response) => {
   database('teams').where('id', request.params.id).select()
-    .then(team => { 
+    .then(team => {
       if (team.length) {
         response.status(200).json(team)
       } else {
@@ -63,3 +64,40 @@ app.get('/api/v1/players/:id', (request, response) => {
       response.status(422).json({ error: error })
     })
 })
+
+app.post('/api/v1/teams', (request, response) => {
+  const team = request.body
+  for (let requiredParameter of ['team_name', 'conference', 'mascot']) {
+    if (!team[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { team_name: <String>, conference: <String>, mascot: <String> }. You're missing a ${requiredParameter} property.` })
+    }
+  }
+  database('teams').insert(team, 'id')
+    .then(team => {
+      response.status(201).json({ id: team[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
+app.post('/api/v1/players', (request, response) => {
+  const player = request.body
+  for (let requiredParameter of ['player_name', 'position', 'new_team', 'contract_value']) {
+    if (!player[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { player_name: <String>, position: <String>, new_team: <String>, contract_value: <String>}. You're missing a ${requiredParameter} property.` })
+    }
+  }
+  database('players').insert(player, 'id')
+    .then(player => {
+      response.status(201).json({ id: player[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
