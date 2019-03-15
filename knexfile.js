c677d2630dee8c27493523ce52d023c6d3b58d0a
Eric Weissman <eric.weissman21@gmail.com>
2019-03-15 09:40:35 -0600

    Adjusts files for Heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,14 @@ module.exports = {
     seeds: {
       directory: './db/seeds/dev'
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
   }
 };
